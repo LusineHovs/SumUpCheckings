bf8944be0456943515e89fb57d8b1ef031dc7eb9
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-03-02 17:23:38 +0400

    Some additions

---
+++ SumUpCheckings/Using/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using
+{
+    class A : IDisposable
+    {
+        public void Method()
+        {
+            Console.WriteLine("Method");
+        }
+        void IDisposable.Dispose()
+        {
+            Console.WriteLine("sd");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (A a = new A())
+            {
+                a.Method();
+            }
+
+            FileStream fs = new FileStream(@"sdfs", FileMode.OpenOrCreate);
+            fs.Dispose();
+
+            A b = new A();
+            ((IDisposable)b).Dispose();
+
+            ;
+        }
+    }
+}
