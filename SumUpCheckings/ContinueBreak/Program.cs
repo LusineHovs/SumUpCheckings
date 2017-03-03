2dee8b120afd94bddb8b4e8db3f566f0e6837fef
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-03-03 12:10:38 +0400

    Additions

---
+++ SumUpCheckings/ContinueBreak/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContinueBreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Method(5);
+        }
+        public static void Method(int a)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                if (i >= 2)
+                    continue;
+            }
+        }
+    }
+}
