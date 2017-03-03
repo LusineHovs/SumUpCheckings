ff0d5e8e1409c116689acf76f5c2faa37349e693
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-03-03 12:08:49 +0400

    Additions

---
+++ SumUpCheckings/Delegate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public delegate void Mydel();
+    class A
+    {
+
+    }
+
+    class Program
+    {
+        public static void Method()
+        {
+            Console.WriteLine("Method1");
+        }
+        public static void Method2()
+        {
+            Console.WriteLine("Method2");
+        }
+        static void Main(string[] args)
+        {
+            Mydel my = new Mydel(Method);
+            my += Method2;
+            my();
+        }
+    }
+}
