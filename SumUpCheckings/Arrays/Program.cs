3ac57a9371a704b262b4f241086e5ccaba895476
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-03-03 12:12:33 +0400

    Additions

---
+++ SumUpCheckings/Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] a = { 2, 5, 8, 7 };
+
+            // Array.Reverse(a);
+            //foreach (var item in a)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            Console.WriteLine(Method("hello"));
+        }
+
+        public static string Method(string a)
+        {
+            char[] b = a.ToCharArray();
+            Array.Reverse(b);
+
+
+            return new string(b);
+        }
+    }
+}
