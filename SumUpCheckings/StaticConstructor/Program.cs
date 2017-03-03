cf1067d7f257d0310ddfad8c0521bf921ea66c81
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-03-03 11:47:35 +0400

    Some additions

---
+++ SumUpCheckings/StaticConstructor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticConstructor
+{
+    class A
+    {
+        private static int a1;
+
+        static A()
+        {
+            a1 = 125;
+            Console.WriteLine(a1);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            A b = new A();
+
+        }
+    }
+}
