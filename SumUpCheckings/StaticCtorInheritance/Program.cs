eb4936e307cd1daecd5340fbef42979cd8cfa800
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-03-03 11:49:35 +0400

    Some additions

---
+++ SumUpCheckings/StaticCtorInheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticCtorInheritance
+{
+    class A
+    {
+        static A()
+        {
+            Console.WriteLine("Base");
+        }
+    }
+    class B : A
+    {
+        static B()
+        {
+            Console.WriteLine("Derived");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+
+        }
+    }
+}
