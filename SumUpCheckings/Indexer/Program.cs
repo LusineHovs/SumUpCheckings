37ad5853cfea7f1317824d97b25099f7d65eeddf
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-03-03 12:06:11 +0400

    Additions

---
+++ SumUpCheckings/Indexer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    class A
+    {
+        private string[] days = { "Sunday", "Monday" };
+
+        public string this[int index]
+        {
+            get { return days[index]; }
+            set { days[index] = value; }
+        }
+    }
+    //static class B
+    //{
+    //    public static void F(this int a)
+    //    {
+    //        Console.WriteLine(a * 10);
+    //    }
+    //}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            Console.WriteLine(a[0]);
+            //int tiv = 10;
+            //tiv.F();
+        }
+
+    }
+}
