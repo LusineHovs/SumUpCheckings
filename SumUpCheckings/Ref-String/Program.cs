cde48d56aa44daba8d172c8c23e6954b019256b3
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-03-03 11:46:27 +0400

    Some additions

---
+++ SumUpCheckings/Ref-String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ref_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string g = "aaa";
+            Console.WriteLine(g);
+            Method(ref g);
+            Console.WriteLine(g);
+
+            Console.WriteLine(@"C:\MyApp\bin\Debug");
+
+            string c = "hello";
+            Console.WriteLine(c.ToUpper());
+            Console.WriteLine(c);
+        }
+
+        public static void Method(ref string a)
+        {
+            a = "bbb";
+            Console.WriteLine(a);
+        }
+    }
+}
