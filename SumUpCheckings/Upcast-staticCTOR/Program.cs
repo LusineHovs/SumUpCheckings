15ffe8d4ed39a2be9b22be4e61e58e228607ae0a
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-03-03 11:53:21 +0400

    Some additions

---
+++ SumUpCheckings/Upcast-staticCTOR/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upcast_staticCTOR
+{
+    class A
+    {
+        public A()
+        {
+            Console.WriteLine("ctorA");
+            F();
+        }
+        public virtual void F()
+        {
+            Console.WriteLine("B.F");
+        }
+
+    }
+    class B : A
+    {
+        static B()
+        {
+            Console.WriteLine("satic ctor b");
+
+        }
+        public override void F()
+        {
+            Console.WriteLine("D.F");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A b = new B();
+        }
+    }
+}
