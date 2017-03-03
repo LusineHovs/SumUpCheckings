d299065834f85930fa76d5a2fb51944a1818861e
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-03-03 12:01:29 +0400

    Some additions

---
+++ SumUpCheckings/InterfaceExplicitImplem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExplicitImplem
+{
+    interface IExample
+    {
+        int Method(int e);
+    }
+    class A : IExample
+    {
+        int IExample.Method(int a)
+        {
+            return a * a;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            ((IExample)a).Method(10);
+        }
+    }
+}
