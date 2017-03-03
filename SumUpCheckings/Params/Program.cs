31b6a878168047cb396688400c60689bec374edf
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-03-03 16:09:37 +0400

    Additions

---
+++ SumUpCheckings/Params/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = Sum(12, 458, 54, 45);
+        }
+        public static int Sum(params int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+
+      
+    }
+}
---
+++ SumUpCheckings/TestP4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestP4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b =Method(15, 17, 5);
+            Console.WriteLine(b);
+        }
+        public static int Method(int a, int k, int p)
+        {
+            int count = 0;
+            int diff = k-a;
+            if (a-k>=0)
+            {
+                return -1;
+            }
+            else
+            {
+                count++;
+                while (diff<=p)
+                {
+                    count++;
+                    p = p - diff;
+                }
+                return count;
+            }
+        }
+    }
+}
