ef257f94418dc131ba9a29120908cd7b49110b5e
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-03-03 11:58:35 +0400

    Some additions

---
+++ SumUpCheckings/Palindrome/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("Deleveled"));
+        }
+        public static bool IsPalindrome(string word)
+        {
+            string temp = word.ToLower();
+            string sum = "";
+            for (int i = temp.Length - 1; i >= 0; i--)
+            {
+                sum += temp[i];
+            }
+            return (sum == temp);
+        }
+    }
+}
