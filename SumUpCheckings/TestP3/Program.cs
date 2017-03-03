bd566a6b0ee9ffe3dbf00b94bbe4b76dccf780ae
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-03-03 11:40:24 +0400

    Some additions

---
+++ SumUpCheckings/TestP3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 1, j = 1;
+            while (++i <= 10)
+            {
+                j++;
+            }
+            Console.WriteLine(i + " " + j);
+        }
+    }
+}
