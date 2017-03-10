43870ed7d1aa8b8715f0868dcfbd5cae1912ed07
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-03-10 11:25:56 +0400

    Additions

---
+++ SumUpCheckings/stackoverflow/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousTypes
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Anonymous types are only "placeholders", it can contain only Properties//
+            var anonymtype = new { Team = "MicArmenia", Duration = 15 };
+        }
+    }
+}
